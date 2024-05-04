ea0baa8314f0f73250ea1499f34956024926deb3
AhmadYousif89 <ayob.4wrk89@gmail.com>
2024-05-04 15:27:39 +0300

    script that sets up web servers for the deployment of web_static

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -7,16 +7,15 @@ if ! command -v nginx &> /dev/null; then
     sudo ufw allow 'Nginx HTTP'
 fi
 
-mkdir -p /data/web_static/releases/test/ /data/web_static/shared/
-touch /data/web_static/releases/test/index.html
-echo "<html><head><title>Test Page</title></head><body><h1>This Is A Testing Page</h1></body></html>" > /data/web_static/releases/test/index.html
-
-ln -s -f /data/web_static/releases/test/ /data/web_static/current
-
+sudo mkdir -p /data/web_static/releases/test/ /data/web_static/shared/
 sudo chown -R ubuntu:ubuntu /data/
 
+sudo touch /data/web_static/releases/test/index.html
+echo "<html><head><title>Test Page</title></head><body><h1>This Is A Testing Page</h1></body></html>" > /data/web_static/releases/test/index.html
+
+sudo ln -s -f /data/web_static/releases/test/ /data/web_static/current
+
 sudo sed -i '/listen 80 default_server/a location /hbnb_static { alias /data/web_static/current/;}' /etc/nginx/sites-enabled/hbnb_static
-# Create symbolic link to enable the site configuration
 sudo ln -s /etc/nginx/sites-available/hbnb_static /etc/nginx/sites-enabled/
 
 sudo service nginx restart
