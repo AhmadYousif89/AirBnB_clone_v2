fcac9ec374f9c5b18aca2fc6a380adbf3018fdac
AhmadYousif89 <ayob.4wrk89@gmail.com>
2024-05-04 15:39:14 +0300

    script that sets up web servers for the deployment of web_static

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -7,12 +7,11 @@ if ! command -v nginx &> /dev/null; then
 fi
 
 sudo mkdir -p /data/web_static/releases/test/ /data/web_static/shared/
-sudo chown -hR ubuntu:ubuntu /data/
-
 sudo touch /data/web_static/releases/test/index.html
 echo "<html><head><title>Test Page</title></head><body><h1>This Is A Testing Page</h1></body></html>" | sudo tee /data/web_static/releases/test/index.html
 
-sudo ln -s -f /data/web_static/releases/test/ /data/web_static/current
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+sudo chown -hR ubuntu:ubuntu /data/
 
 sudo sed -i '38i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
 
