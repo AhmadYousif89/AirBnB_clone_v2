578654058ffaaa3f1f1036bd5da59e02bf930f2e
AhmadYousif89 <ayob.4wrk89@gmail.com>
2024-05-05 06:16:26 +0300

    Fabric script that uploads an archive to the web servers

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,33 +1,37 @@
 #!/usr/bin/python3
 """
-Fabric script that distributes an archive to the web servers
+Fabric script that uploads an archive to the web servers
+execute:
+    fab -f 2-do_deploy_web_static.py do_pack:archive_path=<path> -i <ssh_key>
 """
 
 import os
-from os.path import exists
 from fabric.api import put, run, env
 
+env.user = 'ubuntu'
 env.hosts = ['34.224.62.175', '54.84.8.54']
 
 
 def do_deploy(archive_path):
-    """distributes an archive to the web servers"""
-    if exists(archive_path) is False:
+    """Uploads an archive to the web servers"""
+    if not os.path.exists(archive_path):
         return False
 
-    filename = os.path.basename(archive_path)
-    tmp_tar = f"/tmp/{filename}"
-    name, _ = os.path.splitext(filename)
-    new_folder = f"/data/web_static/releases/{name}"
+    archive_fullname = archive_path.split('/')[-1]
+    archive_tmp_path = f"/tmp/{archive_fullname}"
+    archive_name = archive_fullname.split('.')[0]
+    release_path = f"/data/web_static/releases/{archive_name}"
+
     try:
-        put(archive_path, tmp_tar)
-        run(f"mkdir -p {new_folder}")
-        run(f"tar -xzf {tmp_tar} -C {new_folder}")
-        run(f"rm {tmp_tar}")
-        run(f"mv {new_folder}/web_static/* {new_folder}/")
-        run(f"rm -rf {new_folder}/web_static")
+        put(archive_path, archive_tmp_path)
+        # ex: /data/web_static/releases/web_static_20240505004540
+        run(f"mkdir -p {release_path}")
+        unpack = f'\
+            tar -xzf {archive_tmp_path} -C {release_path} --strip-components=1'
+        run(unpack)
+        run(f"rm {archive_tmp_path}")
         run(f"rm -rf /data/web_static/current")
-        run(f"ln -s {new_folder} /data/web_static/current")
+        run(f"ln -sf {release_path} /data/web_static/current")
         return True
     except Exception:
         return False
