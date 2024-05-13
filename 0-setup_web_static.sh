ea019a0d45a8cadebd85b6fa44ff2655896ffb9f
AhmadYousif89 <ayob.4wrk89@gmail.com>
2024-05-14 01:51:31 +0300

    Update setup_web_static.sh script to install nginx if it is not already installed and add location directive for hbnb_static

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,15 +1,18 @@
 #!/usr/bin/env bash
 # script that sets up web servers for the deployment of web_static
-sudo apt-get update
-sudo apt-get -y install nginx
+if ! command -v nginx &> /dev/null
+then
+    sudo apt update
+    sudo apt install nginx -y
+fi
 
 sudo mkdir -p /data/web_static/releases/test/
 sudo mkdir /data/web_static/shared/
-sudo echo "Hello World!" | sudo tee /data/web_static/releases/test/index.html
+echo "This is a test page!" | sudo tee /data/web_static/releases/test/index.html
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 
 sudo chown -R ubuntu:ubuntu /data/
 
-sudo sed -i '/listen 80 default_server/a location /hbnb_static { alias /data/web_static/current/;}' /etc/nginx/sites-enabled/default
+sudo sed -i '38i\\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
 
 sudo service nginx restart
