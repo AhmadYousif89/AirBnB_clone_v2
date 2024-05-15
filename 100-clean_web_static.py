6bf5e846ff42ee87e8dcd60d44847e6019b44e01
AhmadYousif89 <ayob.4wrk89@gmail.com>
2024-05-16 01:36:37 +0300

    Refactor clean_web_static.py script to improve code readability and maintainability

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 """
 Deletes out-of-date archives
-fab -f 100-clean_web_static.py do_clean:number=2
-    -i ssh-key -u ubuntu > /dev/null 2>&1
+fab -f 100-clean_web_static.py do_clean:number=2 -i ssh-key > /dev/null 2>&1
 """
 
 import os
 from fabric.api import run, cd, lcd, local, env
 
+env.user = 'ubuntu'
 env.hosts = ['54.84.8.54', '34.224.62.175']
 
 
@@ -19,8 +19,8 @@ def do_clean(number=0):
     number is 2, keeps the most and second-most recent archives,
     etc.
     """
-    number = 1 if int(number) == 0 else int(number)
     archives_path = "versions"
+    number = 1 if int(number) == 0 else int(number)
     archives = sorted(os.listdir(archives_path))
     [archives.pop() for i in range(number)]
     with lcd(archives_path):
@@ -29,6 +29,6 @@ def do_clean(number=0):
     releases_path = "/data/web_static/releases"
     with cd(releases_path):
         archives = run("ls -tr").split()
-        archives = [archive for archive in archives if "web_static_" in archive]
-        [archives.pop() for i in range(number)]
+        archives = [archive for archive in archives if "web_static" in archive]
+        [archives.pop() for _ in range(number)]
         [run(f"rm -rf ./{archive}") for archive in archives]
