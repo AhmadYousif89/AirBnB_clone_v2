46714eae4455296f06d6574ba0481e15c6b7e480
AhmadYousif89 <ayob.4wrk89@gmail.com>
2024-05-17 21:01:41 +0300

    Setup for web static files

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -10,7 +10,6 @@ sudo mkdir -p /data/web_static/releases/test/
 sudo mkdir /data/web_static/shared/
 echo "This is a test page!" | sudo tee /data/web_static/releases/test/index.html
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
-
 sudo chown -R ubuntu:ubuntu /data/
 
 sudo sed -i '38i\\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
