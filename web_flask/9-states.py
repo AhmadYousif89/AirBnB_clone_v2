ae80cbfe36b76340647cba649e9a6ebe98dae3fd
AhmadYousif89 <ayob.4wrk89@gmail.com>
2024-05-20 01:09:07 +0300

    Refactor states route and template in 9-states.py

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -20,16 +20,10 @@ def teardown(exception):
 @app.route('/states/<state_id>', strict_slashes=False)
 def states(state_id=None):
     """Displays a list of all States or Cities in a State"""
-    states = list(storage.all("State").values())
+    states = storage.all("State")
     if state_id:
-        for state in states:
-            if state_id == state.id:
-                return render_template(
-                    '9-states.html', state=state, state_id=state_id
-                )
-        else:
-            return render_template('9-states.html')
-    return render_template('9-states.html', states=states)
+        state_id = f"State.{state_id}"
+    return render_template('9-states.html', states=states, state_id=state_id)
 
 
 if __name__ == '__main__':
