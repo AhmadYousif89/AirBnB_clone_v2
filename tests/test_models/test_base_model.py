d4b1a523bbcb68e4ab3adf4d3ec5e9064a3fc996
AhmadYousif89 <ayob.4wrk89@gmail.com>
2024-05-23 15:16:56 +0300

    Refactor test classes for State, Amenity, and City models

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,100 +1,121 @@
 #!/usr/bin/python3
-""" """
-from models.base_model import BaseModel
+"""Test BaseModel class"""
+import time
 import unittest
-import datetime
-import uuid
-import json
-import os
+import unittest.mock
+from datetime import datetime
+from models.base_model import BaseModel
 
 
-class test_basemodel(unittest.TestCase):
+class TestBaseModel(unittest.TestCase):
     """ """
 
-    def __init__(self, *args, **kwargs):
-        """ """
-        super().__init__(*args, **kwargs)
-        self.name = 'BaseModel'
-        self.value = BaseModel
+    def test_instantiation(self):
+        """Test that object is correctly created"""
+        obj = BaseModel()
+        self.assertIs(type(obj), BaseModel)
+        obj.name = "Holberton"
+        obj.number = 89
+        attrs_types = {
+            "id": str,
+            "created_at": datetime,
+            "updated_at": datetime,
+            "name": str,
+            "number": int,
+        }
+        for attr, typ in attrs_types.items():
+            with self.subTest(attr=attr, typ=typ):
+                self.assertIn(attr, obj.__dict__)
+                self.assertIs(type(obj.__dict__[attr]), typ)
+        self.assertEqual(obj.name, "Holberton")
+        self.assertEqual(obj.number, 89)
 
-    def setUp(self):
-        """Setup tests"""
-        self.file = 'hbnb.json'
-
-    def tearDown(self):
-        try:
-            os.remove(self.file)
-        except Exception:
-            pass
-
-    def test_default(self):
-        """ """
-        bm = self.value()
-        self.assertEqual(type(bm), self.value)
-
-    def test_kwargs(self):
-        """ """
-        bm = self.value()
-        copy = bm.to_dict()
-        new = BaseModel(**copy)
-        self.assertFalse(new is bm)
-
-    def test_kwargs_int(self):
-        """ """
-        bm = self.value()
-        copy = bm.to_dict()
-        copy.update({1: 2})
-        with self.assertRaises(TypeError):
-            new = BaseModel(**copy)
-
-    def test_save(self):
-        """Testing save"""
-        bm = self.value()
-        bm.save()
-        key = self.name + "." + bm.id
-        with open('hbnb.json', 'r') as f:
-            j = json.load(f)
-            self.assertEqual(j[key], bm.to_dict())
-
-    def test_str(self):
-        """ """
-        bm = self.value()
-        self.assertEqual(
-            str(bm), '[{}] ({}) {}'.format(self.name, bm.id, bm.__dict__)
-        )
-
-    def test_todict(self):
-        """ """
-        bm = self.value()
-        n = bm.to_dict()
-        self.assertEqual(bm.to_dict(), n)
-
-    def test_kwargs_none(self):
-        """ """
-        n = {None: None}
-        with self.assertRaises(TypeError):
-            new = self.value(**n)
-
-    def test_kwargs_one(self):
-        """ """
-        n = {"Name": "test"}
-        with self.assertRaises(KeyError):
-            new = self.value(**n)
-
-    def test_id(self):
-        """ """
-        new = self.value()
-        self.assertEqual(type(new.id), str)
-
-    def test_created_at(self):
-        """ """
-        new = self.value()
-        self.assertEqual(type(new.created_at), datetime.datetime)
+    def test_datetime_attributes(self):
+        """Test that two BaseModel instances have different datetime objects
+        and that upon creation have identical updated_at and created_at
+        value."""
+        tic = datetime.now()
+        inst1 = BaseModel()
+        toc = datetime.now()
+        self.assertTrue(tic <= inst1.created_at <= toc)
+        time.sleep(1e-4)
+        tic = datetime.now()
+        inst2 = BaseModel()
+        toc = datetime.now()
+        self.assertTrue(tic <= inst2.created_at <= toc)
+        self.assertEqual(inst1.created_at, inst1.updated_at)
+        self.assertEqual(inst2.created_at, inst2.updated_at)
+        self.assertNotEqual(inst1.created_at, inst2.created_at)
 
     def test_updated_at(self):
-        """ """
-        new = self.value()
-        self.assertEqual(type(new.updated_at), datetime.datetime)
-        n = new.to_dict()
-        new = BaseModel(**n)
-        self.assertEqual(new.created_at, new.updated_at)
+        """Test that updated_at attribute is updated"""
+        inst1 = BaseModel()
+        inst2 = BaseModel()
+        self.assertNotEqual(inst1.updated_at, inst2.updated_at)
+
+    def test_uuid(self):
+        """Test that id is a valid uuid"""
+        inst1 = BaseModel()
+        inst2 = BaseModel()
+        for obj in [inst1, inst2]:
+            uuid = str(obj.id)  # Convert uuid to string
+            with self.subTest(uuid=uuid):
+                self.assertIs(type(uuid), str)
+                self.assertRegex(
+                    uuid,
+                    '^[0-9a-f]{8}-[0-9a-f]{4}'
+                    '-[0-9a-f]{4}-[0-9a-f]{4}'
+                    '-[0-9a-f]{12}$',
+                )
+        self.assertNotEqual(inst1.id, inst2.id)
+
+    def test_to_dict(self):
+        """Test conversion of object attributes to dictionary for json"""
+        my_model = BaseModel()
+        my_model.name = "Holberton"
+        my_model.my_number = 89
+        d = my_model.to_dict()
+        expected_attrs = [
+            "id",
+            "created_at",
+            "updated_at",
+            "name",
+            "my_number",
+            "__class__",
+        ]
+        self.assertCountEqual(d.keys(), expected_attrs)
+        self.assertEqual(d['__class__'], 'BaseModel')
+        self.assertEqual(d['name'], "Holberton")
+        self.assertEqual(d['my_number'], 89)
+
+    def test_to_dict_values(self):
+        """test that values in dict returned from to_dict are correct"""
+        t_format = "%Y-%m-%dT%H:%M:%S.%f"
+        bm = BaseModel()
+        new_d = bm.to_dict()
+        self.assertEqual(new_d["__class__"], "BaseModel")
+        self.assertEqual(type(new_d["created_at"]), str)
+        self.assertEqual(type(new_d["updated_at"]), str)
+        self.assertEqual(new_d["created_at"], bm.created_at.strftime(t_format))
+        self.assertEqual(new_d["updated_at"], bm.updated_at.strftime(t_format))
+
+    def test_str(self):
+        """test that the str method has the correct output"""
+        obj = BaseModel()
+        string = "[BaseModel] ({}) {}".format(obj.id, obj.__dict__)
+        self.assertEqual(string, str(obj))
+
+    @unittest.mock.patch('models.storage')
+    def test_save(self, mock_storage):
+        """Test that save method updates `updated_at` and calls
+        `storage.save`"""
+        obj = BaseModel()
+        old_created_at = obj.created_at
+        old_updated_at = obj.updated_at
+        obj.save()
+        new_created_at = obj.created_at
+        new_updated_at = obj.updated_at
+        self.assertNotEqual(old_updated_at, new_updated_at)
+        self.assertEqual(old_created_at, new_created_at)
+        self.assertTrue(mock_storage.new.called)
+        self.assertTrue(mock_storage.save.called)
--- tests/test_models/test_city.py
+++ tests/test_models/test_city.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
-""" """
-from tests.test_models.test_base_model import test_basemodel
+"""Test module for City class"""
+import unittest
+from models.base_model import BaseModel
 from models.city import City
+from models import storage_type
 
 
-class test_City(test_basemodel):
-    """ """
+class TestCity(unittest.TestCase):
+    """Test the City class"""
 
     def __init__(self, *args, **kwargs):
         """ """
@@ -22,3 +24,57 @@ class test_City(test_basemodel):
         """ """
         new = self.value()
         self.assertEqual(type(new.name), str)
+
+    def test_is_subclass(self):
+        """Test that City is a subclass of BaseModel"""
+        city = City()
+        self.assertIsInstance(city, BaseModel)
+        self.assertTrue(hasattr(city, "id"))
+        self.assertTrue(hasattr(city, "created_at"))
+        self.assertTrue(hasattr(city, "updated_at"))
+
+    def test_name_attr(self):
+        """Test that City has attribute name, and it's an empty string"""
+        city = City()
+        self.assertTrue(hasattr(city, "name"))
+        if storage_type == 'db':
+            self.assertEqual(city.name, None)
+        else:
+            self.assertEqual(city.name, "")
+
+    def test_state_id_attr(self):
+        """Test that City has attribute state_id, and it's an empty string"""
+        city = City()
+        self.assertTrue(hasattr(city, "state_id"))
+        if storage_type == 'db':
+            self.assertEqual(city.state_id, None)
+        else:
+            self.assertEqual(city.state_id, "")
+
+    def test_to_dict_creates_dict(self):
+        """test to_dict method creates a dictionary with proper attrs"""
+        c = City()
+        new_d = c.to_dict()
+        self.assertEqual(type(new_d), dict)
+        self.assertFalse("_sa_instance_state" in new_d)
+        for attr in c.__dict__:
+            if attr != "_sa_instance_state":
+                self.assertTrue(attr in new_d)
+        self.assertTrue("__class__" in new_d)
+
+    def test_to_dict_values(self):
+        """test that values in dict returned from to_dict are correct"""
+        t_format = "%Y-%m-%dT%H:%M:%S.%f"
+        c = City()
+        new_d = c.to_dict()
+        self.assertEqual(new_d["__class__"], "City")
+        self.assertEqual(type(new_d["created_at"]), str)
+        self.assertEqual(type(new_d["updated_at"]), str)
+        self.assertEqual(new_d["created_at"], c.created_at.strftime(t_format))
+        self.assertEqual(new_d["updated_at"], c.updated_at.strftime(t_format))
+
+    def test_str(self):
+        """test that the str method has the correct output"""
+        city = City()
+        string = f"[City] ({city.id}) {city.__dict__}"
+        self.assertEqual(string, str(city))
