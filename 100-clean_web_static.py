a7e6ae611fb3fffca0141cae741a2e9a04ab4e80
AhmadYousif89 <ayob.4wrk89@gmail.com>
2024-06-07 19:23:36 +0300

    Update do_clean function to use number parameter instead of keep parameter

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """
 Deletes out-of-date archives
-fab -f 100-clean_web_static.py do_clean:keep=2 -i ssh-key > /dev/null 2>&1
+fab -f 100-clean_web_static.py do_clean:number=2 -i ssh-key > /dev/null 2>&1
 """
 
 import os
@@ -11,7 +11,7 @@ env.user = 'ubuntu'
 env.hosts = ['54.157.181.100', '34.224.62.175']
 
 
-def do_clean(keep=0):
+def do_clean(number=0):
     """Delete out-of-date archives.
     Args:
         keep (int): The number of archives to keep.
@@ -19,15 +19,15 @@ def do_clean(keep=0):
     If (keep) is 2 or more, it will keep the most recent (n of keep) archives.
     """
     archives_path = "versions"
-    keep = 1 if int(keep) == 0 else int(keep)
+    number = 1 if int(number) == 0 else int(number)
     archives = sorted(os.listdir(archives_path))
     with lcd(archives_path):
-        for archive in archives[-keep:]:
+        for archive in archives[-number:]:
             local("rm -rf ./{}".format(archive))
 
     releases_path = "/data/web_static/releases"
     with cd(releases_path):
         output = run("ls -tr").split()
         archives = [archive for archive in output if "web_static" in archive]
-        for archive in archives[-keep:]:
+        for archive in archives[-number:]:
             run("rm -rf ./{}".format(archive))
